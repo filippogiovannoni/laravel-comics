1ad32dd74a932f4985e30acc832bbe24df4ab81b
filippogiovannoni <filippogiovannonii@gmail.com>
2024-04-17 11:22:37 +0200

    add footer blade

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,79 @@
-<footer>footer</footer>
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <div class="footer-left">
+                <div class="left-col">
+                    <ul>
+                        <span>Dc comics</span>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">Tv</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">News</a></li>
+                    </ul>
+                    <ul>
+                        <span>Shop</span>
+                        <li><a href="">Shop dc</a></li>
+                        <li><a href="">Shop dc collectibles</a></li>
+                    </ul>
+                </div>
+                <div class="right-col">
+                    <ul>
+                        <span>Dc</span>
+                        <li><a href="">Terms of use</a></li>
+                        <li><a href="">Privacy policy (new)</a></li>
+                        <li><a href="">Ad Choices</a></li>
+                        <li><a href="">Advertising</a></li>
+                        <li><a href="">Jobs</a></li>
+                        <li><a href="">Subscriptions</a></li>
+                        <li><a href="">Talent Workshops</a></li>
+                        <li><a href="">CPSC Certificates</a></li>
+                        <li><a href="">Ratings</a></li>
+                        <li><a href="">Shop Help</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul>
+                    <ul>
+                        <span>Sites</span>
+                        <li><a href="">DC</a></li>
+                        <li><a href="">MAD Magazine</a></li>
+                        <li><a href="">DC Kids</a></li>
+                        <li><a href="">DC Universe</a></li>
+                        <li><a href="">DC Power Visa</a></li>
+
+                    </ul>
+                </div>
+            </div>
+            <p>All Site Content TM and 2020 DC Entertainment, unless otherwise <a href="">noted here</a>.
+                All rights reserved.
+                <a href="">Cookies Settings</a>
+            </p>
+            <div class="footer-right">
+                <img src="{{ Vite::asset('resources/images/dc-logo-bg.png') }}" alt="dc-logo">
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-bottom-left">
+                <a href="">
+                    <span>Sign-up now!</span>
+                </a>
+            </div>
+            <div class="footer-bottom-right">
+                <span>Follow us</span>
+                <a href=""><img src="{{ Vite::asset('resources/images/footer-facebook.png') }}"
+                        alt="facebook"></a>
+                <a href=""><img src="{{ Vite::asset('resources/images/footer-twitter.png') }}"
+                        alt="twitter"></a>
+                <a href=""><img src="{{ Vite::asset('resources/images/footer-youtube.png') }}"
+                        alt="youtube"></a>
+                <a href=""><img src="{{ Vite::asset('resources/images/footer-pinterest.png') }}"
+                        alt="pinterest"></a>
+                <a href=""><img src="{{ Vite::asset('resources/images/footer-periscope.png') }}"
+                        alt="periscope"></a>
+            </div>
+        </div>
+    </div>
+</footer>
